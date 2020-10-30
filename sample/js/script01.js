3aa2c96175476e6f08d666a26d957eb03fff1cc9
aaaaaaaka <68629495+aaaaaaaka@users.noreply.github.com>
2020-10-30 21:20:53 +0900

    10.30.21:20

--- sample/js/script01.js
+++ sample/js/script01.js
@@ -6,7 +6,26 @@ $(function(){
     console.log(students)
   })
 
- 
+  $("#button01").on("click",function(){
+   $("#content").text("最初のメンバーは"+students[0]+"/人数は"+students.length);
+  })
 
+  $("#button02").on("click",function(){
+    $("#content").text(students[2]);
+  })
+ 
+  $("#button03").on("click",function(){
+    $("#content").text(students[students.length-1]);
+  })
+ 
+  $("#button04").on("click",function(){
+    if(students[length-1]=="山下"){
+
+    }else{
+      students.push("山下")
+    }
+  })
+ 
+  
 
 })
